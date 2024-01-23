c9ecaab3ee392ba8cde32b326b50925f709bafd7
tayfurparmak <parmaktayfur@gmail.com>
2024-01-23 16:01:15 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
           7: "#D1D5DB",
           8: "#E5E7EB",
         },
+      
         primary: "#3758F9",
         "blue-dark": "#1B44C8",
         secondary: "#13C296",
